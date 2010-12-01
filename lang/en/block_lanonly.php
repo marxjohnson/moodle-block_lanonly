3a3fed2eddc05a8136ed866cd128bc8ee619ec93
Mark Johnson <mark.johnson@tauntons.ac.uk>
2010-12-01 11:13:58 +0000

    Modified lang file, block now calls itself On-site/Off-site to make it clearer to non-technical users

---
+++ lang/en/block_lanonly.php
@@ -0,0 +1,9 @@
+<?php
+$string['configtitle']    = 'Configure On-site/Off-site block';
+$string['insidelan']         = 'Content to display to on-site users';
+$string['leaveblanktohide']         = 'Leave title blank to hide title, leave content blank to hide whole block';
+$string['newblock']         = '(New On-site/Off-iste Block)';
+$string['outsidelan']         = 'Content to display to off-site users';
+$string['pluginname']         = 'On-site/Off-site Block';
+$string['pluginnameplural']         = 'On-site/Off-site Blocks';
+?>
