a08e8fa7b48e65355d2c81f56278919da21db475
Mark Johnson <mark.johnson@tauntons.ac.uk>
2011-12-19 13:53:09 +0000

    Added extra metadata to version.php

--- version.php
+++ version.php
@@ -3,3 +3,7 @@
 $plugin->version = 2010112100;
 
 ?>
+$plugin->requires = 2010112400;
+$plugin->component = 'block_lanonly';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '2.0 (Build: 2010112100)';
